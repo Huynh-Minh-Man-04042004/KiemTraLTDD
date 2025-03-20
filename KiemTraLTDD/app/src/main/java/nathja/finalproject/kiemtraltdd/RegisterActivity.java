7031bedb5244b9b94d796f61e7edfb025bcad3ad
vovantri123 <vovantri204@gmail.com>
2025-03-20 15:34:21 +0700

    Fix bug register page

--- KiemTraLTDD/app/src/main/java/nathja/finalproject/kiemtraltdd/RegisterActivity.java
+++ KiemTraLTDD/app/src/main/java/nathja/finalproject/kiemtraltdd/RegisterActivity.java
@@ -2,7 +2,11 @@ package nathja.finalproject.kiemtraltdd;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
 

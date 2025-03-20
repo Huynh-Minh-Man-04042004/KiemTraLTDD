c1b2f7f41dd23dcef7000608c014f07a69af37d0
vovantri123 <vovantri204@gmail.com>
2025-03-20 15:22:51 +0700

    Done câu 1

---
+++ KiemTraLTDD/app/src/main/java/nathja/finalproject/kiemtraltdd/IntroActivity.java
@@ -0,0 +1,37 @@
+package nathja.finalproject.kiemtraltdd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private PrefManager prefManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        prefManager = new PrefManager(this);
+        ConstraintLayout btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (prefManager.isUserLoggedOut()) {
+                    // Chưa đăng nhập -> Chuyển đến LoginActivity
+                    startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+                } else {
+                    // Đã đăng nhập -> Chuyển đến MainActivity
+                    startActivity(new Intent(IntroActivity.this, MainActivity.class));
+                }
+                finish(); // Đóng IntroActivity
+            }
+        });
+    }
+}
--- KiemTraLTDD/app/src/main/java/nathja/finalproject/kiemtraltdd/MainActivity.java
+++ KiemTraLTDD/app/src/main/java/nathja/finalproject/kiemtraltdd/MainActivity.java
@@ -1,6 +1,9 @@
 package nathja.finalproject.kiemtraltdd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +12,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private PrefManager prefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        prefManager = new PrefManager(this);
+        Button btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prefManager.logout();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
 }

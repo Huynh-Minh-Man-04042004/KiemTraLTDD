738e760599d813f992460b3405505cf19f3c244b
Huynh-Minh-Man-04042004 <nsndman0404@gmail.com>
2025-03-20 14:30:26 +0700

    Desing Login Form v1.1

--- KiemTraLTDD/app/build.gradle.kts
+++ KiemTraLTDD/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "nathja.finalproject.kiemtraltdd"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "nathja.finalproject.kiemtraltdd"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
